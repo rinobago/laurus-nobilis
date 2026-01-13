87ed68eb453622c8226afb59b979314558dcc978
Rino Bago <rino.bago@gmail.com>
2026-01-13 22:53:34 +0100

    Suspense fix

--- components/Checkout/Payment/CheckoutPayment.tsx
+++ components/Checkout/Payment/CheckoutPayment.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import BackToBookingButton from "./BackToBookingButton";
 import FormPayment from "./FormPayment";
 import PaymentCard from "./PaymentCard";
@@ -6,7 +7,9 @@ export default function CheckoutPayment() {
     return (
         <section className="flex flex-col items-center bg-beige">
             <div className="flex flex-col gap-64 min-[960px]:gap-24 container items-center text-black">
-                <BackToBookingButton />
+                <Suspense fallback={null}>
+                    <BackToBookingButton />
+                </Suspense>
                 <div className="flex flex-col min-[960px]:flex-row gap-30 w-full justify-center items-center min-[960px]:items-start">
                     <div className="flex flex-col items-center min-[960px]:items-start justify-center gap-[80px] min-[960px]:gap-64">
                         <div className="flex flex-col items-start justify-center gap-12">
